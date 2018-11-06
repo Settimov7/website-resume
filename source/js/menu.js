599c6a06014d41ba145c9ea108ee22a13986a01c
Settimov7 <prytkov.n@yandex.ru>
2018-11-06 22:38:30 +0400

    Рефакторинг, улучшения

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,16 +1,22 @@
 if(document.documentElement.clientWidth <= 767) {
   var header = document.querySelector('.header');
-  console.log(header);
   var mainNavigation = header.querySelector('.main-navigation');
   var mainNavigationToggle = mainNavigation.querySelector('.main-navigation__toggle');
+  var mainNavigationLinks = mainNavigation.querySelectorAll('.main-navigation__menu-link');
 
   header.classList.remove('header--nojs');
   mainNavigation.classList.remove('main-navigation--nojs');
   mainNavigation.classList.add('main-navigation--closed');
 
-  mainNavigationToggle.addEventListener("click", function(evt) {
+  mainNavigationToggle.addEventListener('click', function(evt) {
     evt.preventDefault();
 
     mainNavigation.classList.toggle('main-navigation--closed');
   });
+
+  for(var i = 0; i < mainNavigationLinks.length; i++) {
+    mainNavigationLinks[i].addEventListener('click', function(evt) {
+      mainNavigation.classList.add('main-navigation--closed');
+    });
+  }
 }
