4d04366c0afd921b3de15b972e53e832c7f72e67
Settimov7 <prytkov.n@yandex.ru>
2018-11-16 14:40:16 +0400

    Добавил в сборку папку download

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,8 @@ gulp.task("copy", function() {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**/*.{png,jpg,svg,webp}",
-    "source/projects/**/*"
+    "source/projects/**/*",
+    "source/download/**/*"
   ], {
     base: "source"
   })
